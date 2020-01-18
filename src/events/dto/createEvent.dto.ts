4e17c5c3627abde98699649105556fc2cb1b1fda
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-01-18 02:12:04 +0100

    Added input validation

--- src/events/dto/createEvent.dto.ts
+++ src/events/dto/createEvent.dto.ts
@@ -1,4 +1,11 @@
+import { IsOptional, IsString, IsEmpty, IsNotEmpty } from "class-validator";
 export class CreateEventDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+  @IsString()
+  @IsNotEmpty()
   description: string;
+  @IsOptional()
+  isPublished: boolean;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
-import { Logger } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
 dotenv.config();
 const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
   await app.listen(port);
 }
 bootstrap();
